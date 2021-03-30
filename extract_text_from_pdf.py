bdb680cd23cf5fe86b8626dc9fe322b2a58f71be
gunnar <gunnar.aronsen@tronderenergi.no>
2021-03-30 17:06:26 +0200

    Back-up

--- extract_text_from_pdf.py
+++ extract_text_from_pdf.py
@@ -2,12 +2,31 @@ import os
 import ocrmypdf
 
 
-if __name__ == '__main__':
-    input_file_name = 'results/FeilListe_cropped.pdf'
-    ocr_file_name = 'results/FeilListe_ocr_cropped.pdf'
-    txt_file_name = 'results/FeilListe_ocr_cropped.txt'
-    os.system(f'ocrmypdf  --deskew --force-ocr --oversample 1200 --sidecar {txt_file_name} {input_file_name}'
+if __name__ == "__main__":
+    # input_file_name = "tests/fifty_pages_error_list_cropped.pdf"
+    # ocr_file_name = "tests/fifty_pages_ocr_error_list_cropped.pdf"
+    # txt_file_name = "tests/fifty_pages_ocr_error_list_cropped.txt"
+    input_file_name = "results/FeilListe_cropped.pdf"
+    ocr_file_name = "results/FeilListe_ocr_cropped.pdf"
+    txt_file_name = "results/FeilListe_ocr_cropped.txt"
+
+
+    os.system(f'ocrmypdf  '
+              f'--deskew '
+              f'--force-ocr '
+              f'--oversample 600 '
+              f'--sidecar {txt_file_name}'
+              f' {input_file_name}'
               f' {ocr_file_name}')
 
-
-
+    # os.system(
+    #     f"ocrmypdf --verbose 1 "
+    #     f"-l -eng "
+    #     f"--deskew "
+    #     f"--force-ocr"
+    #     f"--oversample 600"
+    #     f" --sidecar"
+    #     f" {txt_file_name}"
+    #     f" {input_file_name}"
+    #     f" {ocr_file_name}"
+    # )
